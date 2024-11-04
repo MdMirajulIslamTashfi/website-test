7490d6898db2bd55c148553a272aea75468dee2c
Md. Mirajul Islam Tashfi <mirajul.tashfi12@gmail.com>
2024-11-04 15:10:22 +0600

    Initital Commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,3 @@
+<h1>Welcome to SvelteKit</h1>
+<div class="text-neutral-900 font-bold italic">Hello there</div>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
